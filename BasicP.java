0a7f182bb1dba112ef3e39388dafd24ca89ed4d3
SandeepRampha <r.sandeep2421@gmail.com>
2024-01-23 16:24:06 +0530

    This is my second git

---
+++ BasicP.java
@@ -0,0 +1,100 @@
+/*Find Simple Interest for the following values SI = PTR/100; P=1000, T=2, R= 3 
+class intrest
+{
+  public static void main(String args[])
+  { 
+    int p=1000,t=2,r=3;
+    int SI = p*t*r/100;
+    System.out.println("Simple Intrest is: "+SI);
+  }
+}*/
+
+
+/*The length & breadth of a rectangle and radius of a circle are input through the keyboard. Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.
+class radius
+{
+  public static void main(String args[])
+  { 
+    int l=4,w=6,r=7;
+    int ARec=l*w;
+    int PRec=(l+w)*2;
+    double ACir=3.14*r*r;
+    double CCircle=2*3.14*r;
+    System.out.println("Area of Rectangle is: "+ARec);
+    System.out.println("Primeter of Rectangle is: "+PRec);
+    System.out.println("Area of Circle is: "+ACir);
+    System.out.println("Circumference of circle is: "+CCircle); 
+  }
+}*/
+
+/*Implement a Product Application(Use all 8 data types)
+class PAPP
+{
+  public static void main(String args[])
+  { 
+   String Pname="Laptop";
+   char Pcode='A';
+   short quantity=100;
+   double price=35000.73;
+   int PhoneNO=1234567890;
+   byte gateNO=3;
+   long serialNO=12345678945446L;
+   float digit=1.22f;
+   System.out.println(Pname);
+   System.out.println(Pcode);
+   System.out.println(quantity);
+   System.out.println(price);
+   System.out.println(PhoneNO);
+   System.out.println(gateNO);
+   System.out.println(serialNO);
+   System.out.println(digit);
+  }
+}*/
+
+        
+/*If cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss. Also determine how much profit he made or loss he incurred.
+
+
+class ProfitLoss
+{
+  public static void main(String args[])
+  {
+
+    int costprice=100;
+    int sellingprice=80; 
+    int profitLoss=sellingprice-costprice;
+
+
+        if (profitLoss > 0) 
+        {
+        System.out.printf("Profit is:"+profitLoss);
+        } 
+        else if (profitLoss < 0) 
+        {
+        System.out.printf("Loss is:"+profitLoss);
+        } 
+        else 
+        {
+        System.out.println("No profit, no loss");
+        }
+   }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
